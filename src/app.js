7543b540f23e0bf78aeab38966be559b97a34b2a
Deyvi Joel Xol <xdave418@gmail.com>
2026-01-29 18:46:18 +0000

    Initial backend setup with Express

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    status: 'OK',
+    message: 'Mini proyecto funcionando'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
